25b6dbcda4d607b4a3a5be9ef420ec405995bdd9
bartosz.lagoda <composit095@gmail.com>
2025-05-24 15:36:20 +0200

    Create class 'Koszyk' and 'Produkt'

--- src/main/java/org/example/Produkt.java
+++ src/main/java/org/example/Produkt.java
@@ -1,5 +1,25 @@
 package org.example;
 
 public class Produkt {
+    private String nazwa;
+    private double cena;
+    private String kategoria;
+    private boolean aktywny;
 
+    public Produkt(String nazwa, double cena, String kategoria, boolean aktywny){
+        this.nazwa = nazwa;
+        this.cena = cena;
+        this.kategoria = kategoria;
+        this.aktywny = aktywny;
+    }
+
+    @Override
+    public String toString() {
+        return "Produkt{" +
+                "nazwa='" + nazwa + '\'' +
+                ", cena=" + cena +
+                ", kategoria='" + kategoria + '\'' +
+                ", aktywny=" + aktywny +
+                '}';
+    }
 }
