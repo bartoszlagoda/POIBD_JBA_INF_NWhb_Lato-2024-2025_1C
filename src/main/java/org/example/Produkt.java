44d79d6023e48263cc6621a237428fea40f8e14f
bartoszlagoda <composit095@gmail.com>
2025-06-13 11:39:57 +0200

    Dodanie klasy obsługującej Produkt

--- src/main/java/org/example/Produkt.java
+++ src/main/java/org/example/Produkt.java
@@ -3,23 +3,40 @@ package org.example;
 public class Produkt {
     private String nazwa;
     private double cena;
-    private String kategoria;
+    private Kategoria kategoria;
+    private int stan;
     private boolean aktywny;
 
-    public Produkt(String nazwa, double cena, String kategoria, boolean aktywny){
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public Kategoria getKategoria() {
+        return kategoria;
+    }
+
+    public int getStan() {
+        return stan;
+    }
+
+    public boolean isAktywny() {
+        return aktywny;
+    }
+
+    public Produkt(String nazwa, double cena, Kategoria kategoria, int stan, boolean aktywny){
         this.nazwa = nazwa;
         this.cena = cena;
         this.kategoria = kategoria;
+        this.stan = stan;
         this.aktywny = aktywny;
     }
 
     @Override
     public String toString() {
-        return "Produkt{" +
-                "nazwa='" + nazwa + '\'' +
-                ", cena=" + cena +
-                ", kategoria='" + kategoria + '\'' +
-                ", aktywny=" + aktywny +
-                '}';
+        return nazwa + " - " + cena + " zł";
     }
 }
